b984ae8502a209c992a8e62087efa446f7104e8e
YuutaMatuda <do_re_3_fa@yahoo.co.jp>
2023-08-30 15:53:43 +0900

    add-CRUD

--- resources/js/Components/FlashMessage.vue
+++ resources/js/Components/FlashMessage.vue
@@ -2,5 +2,9 @@
 </script>
 
 <template>
-    <div v-if="$page.props.flash.status === 'success'" class="bg-blue-300 text-white p-4">登録完了</div>
+    <div v-if="$page.props.flash.status === 'success'" class="bg-blue-300 text-white p-4">{{ $page.props.flash.message }}
+    </div>
+
+    <div v-if="$page.props.flash.status === 'danger'" class="bg-red-300 text-white p-4">{{ $page.props.flash.message }}
+    </div>
 </template>
